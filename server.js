4f3cc0f721081278205e4c9ecbcfcaf3a0c7ab53
Brad McKinney <brad.mckin@gmail.com>
2019-01-25 13:47:41 -1000

    loads in middleware

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 const database = {};
 
-
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const server = app.listen(PORT, () => {
   console.log(`Running on PORT ${PORT}`);
