fa058036f00c3d9addb6b9cc9b59f81eebbe4e2e
Brad McKinney <brad.mckin@gmail.com>
2019-01-25 13:44:27 -1000

    instantiates basic express server

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 'use-strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+const database = {};
 
 
+
+const server = app.listen(PORT, () => {
+  console.log(`Running on PORT ${PORT}`);
+}); 
